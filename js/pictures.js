63115ffb88880a3743be7a7a6e3b5052017883b4
kuhel <vorontsov.gleb@gmail.com>
2016-02-09 19:59:33 +0300

    Filtering added

--- js/pictures.js
+++ js/pictures.js
@@ -1,4 +1,3 @@
-/* global pictures: true */
 
 /**
  * @fileoverview
@@ -9,21 +8,32 @@
 
 
 (function() {
+  var pictures = [];
   var picturesContainer = document.querySelector('.pictures');
   var filtersContainer = document.querySelector('.filters');
+  var activeFilter = 'filter-popular';
+  var filters = document.querySelectorAll('.filters-radio');
+
+  for (var i = 0; i < filters.length; i++) {
+    filters[i].onclick = function(event) {
+      var clickedFilterID = event.target.id;
+      setActiveFilter(clickedFilterID);
+    };
+  }
+
   filtersContainer.classList.remove('hidden');
 
 
 
   /**
    * Отрисовка списка картинок
-   * @param {Array.<Object>} pictures
+   * @param {Array.<Object>} picturesToDraw
    */
-  function renderPictures (pictures) {
+  function renderPictures(picturesToDraw) {
     picturesContainer.innerHTML = '';
     var fragment = document.createDocumentFragment();
 
-    pictures.forEach(function(picture) {
+    picturesToDraw.forEach(function(picture) {
       var element = getElementFromTemplate(picture);
       fragment.appendChild(element);
     });
@@ -31,25 +41,60 @@
     picturesContainer.appendChild(fragment);
   }
 
+  /**
+   * Установка выбранного фильтра
+   * @param {string} id
+   */
+  function setActiveFilter(id) {
+    var filteredPictures = pictures.slice(0);
+    if (activeFilter === id) {
+      return;
+    }
+
+    document.querySelector('#' + activeFilter).setAttribute('checked', 'false');
+    document.querySelector('#' + id).setAttribute('checked', 'true');
+
+    if (id === 'filter-new') {
+      filteredPictures = filteredPictures.sort(function(a, b) {
+        return new Date(b.date) - new Date(a.date);
+      });
+    } else if (id === 'filter-discussed') {
+      filteredPictures = filteredPictures.sort(function(a, b) {
+        return b.comments - a.comments;
+      });
+    } else if (id === 'filter-popular') {
+      filteredPictures = filteredPictures.sort(function(a, b) {
+        return b.likes - a.likes;
+      });
+    }
+
+    renderPictures(filteredPictures);
+
+  }
+
 
   /**
-   * Получаем списко картинок с сервера
+   * Получаем список картинок с сервера
    */
   function getPictures() {
     var XHRequest = new XMLHttpRequest();
 
-    XHRequest.open = ('GET', 'http://o0.github.io/assets/json/pictures.json');
-    XHRequest.onload = function (event) {
-var rawData = event.target.response;
-      var loadedPictures = JSON.parse(rawData);
+    XHRequest.open('GET', 'http://o0.github.io/assets/json/pictures.json');
+    XHRequest.timeout = 10000;
+    document.querySelector('.pictures').classList.add('pictures-loading');
+    XHRequest.onload = function(event) {
+      var rawData = event.target.response;
+      pictures = JSON.parse(rawData);
+      document.querySelector('.pictures').classList.remove('pictures-loading');
+      renderPictures(pictures);
+    };
 
-      renderPictures(loadedPictures);
-    }
-
-    XHRequest.send;
+    XHRequest.send();
   }
 
   getPictures();
+
+
 
   /**
    * Создаем новый DOM элемент на основе шаблона
@@ -60,19 +105,19 @@ var rawData = event.target.response;
     var templateSelector = 'picture-template';
     var template = document.getElementById(templateSelector);
     var element;
+    var ELEMENT_IMAGE_WIDTH = 182;
+    var ELEMENT_IMAGE_HEIGHT = 182;
+    var elementImage = new Image(ELEMENT_IMAGE_WIDTH, ELEMENT_IMAGE_HEIGHT);
 
     if ('content' in template) {
       element = template.content.childNodes[1].cloneNode(true);
     } else {
       element = template.childNodes[1].cloneNode(true);
     }
+    var templateImage = element.querySelector('img');
 
-    var ELEMENT_IMAGE_WIDTH = 182;
-    var ELEMENT_IMAGE_HEIGHT = 182;
-    var elementImage = new Image(ELEMENT_IMAGE_WIDTH, ELEMENT_IMAGE_HEIGHT);
     elementImage.src = data.url;
 
-    var templateImage = element.querySelector('img');
 
     var loadErrorTimeout = setTimeout(function() {
       element.classList.add('picture-load-failure');
@@ -94,5 +139,7 @@ var rawData = event.target.response;
 
     return element;
   }
+
+
 
 })();
