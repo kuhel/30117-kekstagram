361eb01bf94b7a1e4d583b70a8ede92d929ba8ed
kuhel <vorontsov.gleb@gmail.com>
2016-02-26 22:44:07 +0300

    no message

---
+++ js/photo-preview.js
@@ -0,0 +1,16 @@
+/* global inherit: true, PhotoBase: true */
+
+/**
+ * @fileoverview
+ * @author Gleb Vorontsov
+ */
+
+'use strict';
+
+(function() {
+
+  function PhotoPreview() {}
+
+  inherit(PhotoPreview, PhotoBase);
+
+})();
